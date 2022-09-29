5336c3cbdac4e6d8ae262dcded1bc1aba1ed0f60
Strevia <phillipnissen7@gmail.com>
2022-09-29 16:26:07 -0400

    Fix Milestone 1

--- app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.kt
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.kt
@@ -13,7 +13,7 @@ class CurrentDayActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_current_day)
         tvDay = findViewById<View>(R.id.tvDay) as TextView
-        tvDay?.setText(dayOfMonth)
+        tvDay?.text = dayOfMonth.toString()
     }
 
     private val dayOfMonth: Int
